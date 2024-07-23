acde9fd29b732df30037bb8ef1120fd6d910def0
Owen Prosser <owenprosser@icloud.com>
2024-07-23 19:55:29 +0100

    Fix dashboard welcome

--- app/(app)/dashboard/page.tsx
+++ app/(app)/dashboard/page.tsx
@@ -6,16 +6,19 @@ import Transactions from "./_components";
 import TotalsLoading from "./_components/totals-loading";
 import { Suspense } from "react";
 import TransactionsLoading from "./_components/index-loading";
+import { getSession } from "@/lib/auth";
 
 export const metadata: Metadata = { title: "Dashboard" };
 
 export const runtime = "edge";
 
-const DashboardPage = () => {
+const DashboardPage = async () => {
+  const session = await getSession();
+
   return (
     <>
       <Caption>{format(new Date(), "EEEE, do MMMM yyyy")}</Caption>
-      <Title>Welcome back, Owen</Title>
+      <Title>Welcome back, {session?.user.firstName}</Title>
 
       <Suspense fallback={<TotalsLoading />}>
         <Totals />
