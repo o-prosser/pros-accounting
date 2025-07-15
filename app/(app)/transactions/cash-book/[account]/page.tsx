37fa59b006722c1e1fc312a2726853da6929c819
Owen Prosser <owenprosser@icloud.com>
2025-07-15 20:08:39 +0100

    Ignore

--- app/(app)/transactions/cash-book/[account]/page.tsx
+++ app/(app)/transactions/cash-book/[account]/page.tsx
@@ -100,6 +100,7 @@ const TransactionsPage = async (props: {
         columns={account ? columnsWithoutAccount : columns}
         data={balancedTransfersPayments.reverse()}
         categories={categories}
+        // @ts-ignore
         categoryKey="categoryId"
       />
     </>
