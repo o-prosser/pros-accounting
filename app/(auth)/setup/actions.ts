1a4e5e1897a5046d75f4a2b42062b7eb274fe5d3
Owen Prosser <owenprosser@icloud.com>
2025-07-15 10:49:26 +0100

    Server action fix 2

--- app/(auth)/setup/actions.ts
+++ app/(auth)/setup/actions.ts
@@ -14,52 +14,67 @@ import { z } from "zod";
 
 const schema = z.object({
   name: z.string().min(3).max(50),
-})
+});
 
 export const setupAction = async (formData: FormData) => {
   const fields = schema.safeParse(Object.fromEntries(formData));
 
   if (!fields.success) {
-    return {
-      error: fields.error.flatten().fieldErrors,
-    }
+    // return {
+    //   error: fields.error.flatten().fieldErrors,
+    // }
+
+    throw new Error();
   }
 
   try {
     const session = await getSession();
     if (!session) throw new Error();
 
-    if (fields.data.name.length === 36 && fields.data.name.split("-").length === 5) {  
-      await db.update(usersTable).set({
-        organisationId: fields.data.name,
-      }).where(eq(usersTable.id, session.user.id));
+    if (
+      fields.data.name.length === 36 &&
+      fields.data.name.split("-").length === 5
+    ) {
+      await db
+        .update(usersTable)
+        .set({
+          organisationId: fields.data.name,
+        })
+        .where(eq(usersTable.id, session.user.id));
     } else {
-      const organisation = await db.insert(organisationsTable).values({
-        name: fields.data.name,
-        slug: fields.data.name.toLowerCase().trim().replace(" ", "-"),
-        ownerId: session.user.id,
-      }).returning({id: organisationsTable.id})
-  
-      await db.update(usersTable).set({
-        organisationId: organisation[0].id,
-      }).where(eq(usersTable.id, session.user.id));
-    }
+      const organisation = await db
+        .insert(organisationsTable)
+        .values({
+          name: fields.data.name,
+          slug: fields.data.name.toLowerCase().trim().replace(" ", "-"),
+          ownerId: session.user.id,
+        })
+        .returning({ id: organisationsTable.id });
 
-  } catch (error) {
-    if (error instanceof DrizzleError && error.message === "AUTH_DUPLICATE_KEY_ID") {
-      return {
-        error: {
-          email: ["A user with this email already exists."],
-        }
-      }
-    } else {
-      return {
-        error: {
-          email: ["An error occurred. Please try again"]
-        }
-      }
+      await db
+        .update(usersTable)
+        .set({
+          organisationId: organisation[0].id,
+        })
+        .where(eq(usersTable.id, session.user.id));
     }
+  } catch (error) {
+    throw error;
+
+    // if (error instanceof DrizzleError && error.message === "AUTH_DUPLICATE_KEY_ID") {
+    //   return {
+    //     error: {
+    //       email: ["A user with this email already exists."],
+    //     }
+    //   }
+    // } else {
+    //   return {
+    //     error: {
+    //       email: ["An error occurred. Please try again"]
+    //     }
+    //   }
+    // }
   }
 
-  redirect("/dashboard")
-}
+  redirect("/dashboard");
+};
