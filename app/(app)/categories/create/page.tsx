7fb1649e070e6120c0aa2f6d3696ffb01a90cb09
Owen Prosser <owenprosser@icloud.com>
2025-07-27 15:44:20 +0100

    Show form on standalone page

--- app/(app)/categories/create/page.tsx
+++ app/(app)/categories/create/page.tsx
@@ -1,4 +1,5 @@
 import BackButton from "@/components/back-button";
+import AddCategoryForm from "../(main)/@modal/(.)create/_components/form";
 
 const AddCategoryPage = () => {
   return (
@@ -8,10 +9,7 @@ const AddCategoryPage = () => {
       <div className="rounded-2xl border bg-muted/50 p-3">
         <h3 className="font-medium text-xl flex-1 mb-3">Add category</h3>
 
-        {/* <AddPaymentForm
-          searchParams={await searchParams}
-          categories={categories}
-        /> */}
+        <AddCategoryForm />
       </div>
     </>
   );
