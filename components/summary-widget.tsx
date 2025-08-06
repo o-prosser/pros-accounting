fcdfd50e3f0bb2e43a6d1be127ba1f74b7a100ea
Owen Prosser <owenprosser@icloud.com>
2025-08-06 11:49:53 +0100

    Context menu on dashboard

--- components/summary-widget.tsx
+++ components/summary-widget.tsx
@@ -19,6 +19,8 @@ import {
   addDays,
   addMonths,
   differenceInMonths,
+  endOfMonth,
+  format,
   getMonth,
   getYear,
   isBefore,
@@ -28,6 +30,23 @@ import {
 } from "date-fns";
 import IncomeExpenseChart from "./summary-chart";
 import { cn } from "@/lib/utils";
+import {
+  ContextMenu,
+  ContextMenuCheckboxItem,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuLabel,
+  ContextMenuRadioGroup,
+  ContextMenuRadioItem,
+  ContextMenuRadioItemLink,
+  ContextMenuSeparator,
+  ContextMenuShortcut,
+  ContextMenuSub,
+  ContextMenuSubContent,
+  ContextMenuSubTrigger,
+  ContextMenuTrigger,
+} from "./ui/context-menu";
+import LoadingIndicator from "./loading-indicator";
 
 const getMonths = async () => {
   const organisation = await selectCurrentOrganisation();
@@ -161,6 +180,8 @@ const SummaryWidget = async ({
   });
   const transfers = await selectTransfers({});
 
+  const organisation = await selectCurrentOrganisation();
+
   const income = getTotal({
     transactions: transactions.filter((t) =>
       currentFinancialYear
@@ -239,93 +260,149 @@ const SummaryWidget = async ({
   });
 
   return (
-    <div className="rounded-2xl p-3 border bg-muted/50 group">
-      <div className="flex gap-2 items-center">
-        <div
-          className={cn(
-            "bg-gradient-to-br size-7 rounded-lg grid place-items-center via-50%",
-            account === "charity" &&
-              "via-orange-600 from-orange-400 to-orange-400",
-            account === "club" && "via-cyan-600 from-cyan-400 to-cyan-400",
-          )}
-        >
-          <BadgePoundSterlingIcon className="size-4 text-background" />
-        </div>
-        <h3 className="font-medium text-xl flex-1">
-          <span className="capitalize">{account}</span> account
-        </h3>
+    <ContextMenu>
+      <ContextMenuTrigger>
+        <div className="rounded-2xl p-3 border bg-muted/50 group">
+          <div className="flex gap-2 items-center">
+            <div
+              className={cn(
+                "bg-gradient-to-br size-7 rounded-lg grid place-items-center via-50%",
+                account === "charity" &&
+                  "via-orange-600 from-orange-400 to-orange-400",
+                account === "club" && "via-cyan-600 from-cyan-400 to-cyan-400",
+              )}
+            >
+              <BadgePoundSterlingIcon className="size-4 text-background" />
+            </div>
+            <h3 className="font-medium text-xl flex-1">
+              <span className="capitalize">{account}</span> account
+            </h3>
 
-        <Button
-          size="icon"
-          variant="ghost"
-          className="size-7 opacity-0 group-hover:opacity-100 transition"
-          asChild
-        >
+            <Button
+              size="icon"
+              variant="ghost"
+              className="size-7 opacity-0 group-hover:opacity-100 transition"
+              asChild
+            >
+              <Link href={`/cashbook?account=${account}`}>
+                <ExternalLinkIcon />
+              </Link>
+            </Button>
+          </div>
+          <div className="bg-background border rounded-lg p-3 mt-2 relative overflow-hidden">
+            <div className="absolute -left-px -top-px right-0 bottom-0 bg-[linear-gradient(to_right,#73737320_1px,transparent_1px),linear-gradient(to_bottom,#73737320_1px,transparent_1px)] bg-[size:20px_20px]" />
+            <div className="absolute inset-0 h-full w-full bg-gradient-to-bl from-transparent via-background via-70% to-background"></div>
+            <div className="grid grid-cols-2 relative">
+              <div>
+                <p className="text-2xl font-mono font-semibold tracking-tight">
+                  {incomeForBalance + initial - expenseForBalance == 0
+                    ? "---"
+                    : currency(incomeForBalance + initial - expenseForBalance)}
+                </p>
+                <p className="font-medium text-muted-foreground">
+                  Current balance
+                </p>
+              </div>
+
+              <div>
+                <div className="flex items-center gap-2 mb-1">
+                  <BanknoteArrowUpIcon
+                    className={cn(
+                      "size-4",
+                      account === "charity" && "text-orange-600",
+                      account === "club" && "text-cyan-600",
+                    )}
+                  />
+                  <div className="flex items-end gap-1">
+                    <p className="font-mono font-medium tracking-tight">
+                      {income == 0 ? "---" : currency(income)}
+                    </p>
+                    <p className="text-sm pb-[0.5px] text-muted-foreground">
+                      income
+                    </p>
+                  </div>
+                </div>
+                <div className="flex items-center gap-2">
+                  <ShoppingBagIcon
+                    className={cn(
+                      "size-4",
+                      account === "charity" && "text-orange-600",
+                      account === "club" && "text-cyan-600",
+                    )}
+                  />
+                  <div className="flex items-end gap-1">
+                    <p className="font-mono font-medium tracking-tight">
+                      {expense == 0 ? "---" : currency(expense)}
+                    </p>
+                    <p className="text-sm pb-[0.5px] text-muted-foreground">
+                      expenses
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="bg-background border rounded-lg p-3 mt-2">
+            <IncomeExpenseChart
+              min={min}
+              data={monthlyTotals[account]}
+              account={account}
+            />
+          </div>
+        </div>
+      </ContextMenuTrigger>
+      <ContextMenuContent className="w-52">
+        <ContextMenuItem inset asChild>
           <Link href={`/cashbook?account=${account}`}>
-            <ExternalLinkIcon />
+            Open cash book
+            <LoadingIndicator />
           </Link>
-        </Button>
-      </div>
-      <div className="bg-background border rounded-lg p-3 mt-2 relative overflow-hidden">
-        <div className="absolute -left-px -top-px right-0 bottom-0 bg-[linear-gradient(to_right,#73737320_1px,transparent_1px),linear-gradient(to_bottom,#73737320_1px,transparent_1px)] bg-[size:20px_20px]" />
-        <div className="absolute inset-0 h-full w-full bg-gradient-to-bl from-transparent via-background via-70% to-background"></div>
-        <div className="grid grid-cols-2 relative">
-          <div>
-            <p className="text-2xl font-mono font-semibold tracking-tight">
-              {incomeForBalance + initial - expenseForBalance == 0
-                ? "---"
-                : currency(incomeForBalance + initial - expenseForBalance)}
-            </p>
-            <p className="font-medium text-muted-foreground">Current balance</p>
-          </div>
-
-          <div>
-            <div className="flex items-center gap-2 mb-1">
-              <BanknoteArrowUpIcon
-                className={cn(
-                  "size-4",
-                  account === "charity" && "text-orange-600",
-                  account === "club" && "text-cyan-600",
-                )}
-              />
-              <div className="flex items-end gap-1">
-                <p className="font-mono font-medium tracking-tight">
-                  {income == 0 ? "---" : currency(income)}
-                </p>
-                <p className="text-sm pb-[0.5px] text-muted-foreground">
-                  income
-                </p>
-              </div>
-            </div>
-            <div className="flex items-center gap-2">
-              <ShoppingBagIcon
-                className={cn(
-                  "size-4",
-                  account === "charity" && "text-orange-600",
-                  account === "club" && "text-cyan-600",
-                )}
-              />
-              <div className="flex items-end gap-1">
-                <p className="font-mono font-medium tracking-tight">
-                  {expense == 0 ? "---" : currency(expense)}
-                </p>
-                <p className="text-sm pb-[0.5px] text-muted-foreground">
-                  expenses
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="bg-background border rounded-lg p-3 mt-2">
-        <IncomeExpenseChart
-          min={min}
-          data={monthlyTotals[account]}
-          account={account}
-        />
-      </div>
-    </div>
+        </ContextMenuItem>
+        <ContextMenuItem inset asChild>
+          <Link href={`/cashbook/create?account=${account}`}>
+            Add payment
+            <LoadingIndicator />
+            <ContextMenuShortcut>⌘N</ContextMenuShortcut>
+          </Link>
+        </ContextMenuItem>
+        <ContextMenuSub>
+          <ContextMenuSubTrigger inset>Reports</ContextMenuSubTrigger>
+          <ContextMenuSubContent className="w-64">
+            <ContextMenuItem asChild>
+              <Link
+                href={`/reports/account-summary/report?charity=on&club=on&from=${format(
+                  startOfMonth(new Date()),
+                  "yyyy-MM-dd",
+                )}&to=${format(endOfMonth(new Date()), "yyyy-MM-dd")}`}
+              >
+                Generate monthly summary
+                <LoadingIndicator />
+              </Link>
+            </ContextMenuItem>
+          </ContextMenuSubContent>
+        </ContextMenuSub>
+        <ContextMenuSeparator />
+        <ContextMenuRadioGroup
+          value={
+            currentFinancialYear?.id ||
+            organisation.financialYears.find((fy) => fy.isCurrent)?.id
+          }
+        >
+          <ContextMenuLabel inset>Financial year</ContextMenuLabel>
+          {organisation.financialYears.toReversed().map((fy, key) => (
+            <ContextMenuRadioItemLink
+              key={key}
+              value={fy.id}
+              href={`/dashboard?fy=${fy.id}`}
+            >
+              {format(fy.startDate, "MMM yyyy")} &ndash;{" "}
+              {format(fy.endDate, "MMM yyyy")}
+            </ContextMenuRadioItemLink>
+          ))}
+        </ContextMenuRadioGroup>
+      </ContextMenuContent>
+    </ContextMenu>
 
     // <Card
     //   className={cn(
